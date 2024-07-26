2fc89f3f706a0e222643a3f27f2c4f68a705b512
minaripenguin <minaripenguin@users.noreply.github.com>
2024-07-26 18:12:03 +0800

    Add quickswitch lawnchair hooks
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -399,6 +399,14 @@ PRODUCT_PACKAGES += \
     Lawnchair \
     LawnchairOverlay \
     Lawnicons
+
+# Lawnchair Launcher
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.quickswitch_lawnchair_shipped=1
+else 
+# Lawnchair Launcher
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.quickswitch_lawnchair_shipped=0
 endif
 
 # Include {Lato,Rubik} fonts
