55dd95f006b239e2b2e2d671a99c70bc49a449c9
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-10 21:39:26 +0800

    Ship BCR by default

--- config.mk
+++ config.mk
@@ -410,6 +410,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     persist.sys.quickswitch_lawnchair_shipped=0
 endif
 
+TARGET_PREBUILT_BCR ?= true
 # Basic call recorder
 ifeq ($(strip $(TARGET_PREBUILT_BCR)),true)
 PRODUCT_PACKAGES += \
