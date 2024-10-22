6336ae561a9d28805d006fbc95a1038427d43ea1
cjh1249131356 <cjh1249131356@gmail.com>
2024-10-22 22:48:57 +0800

    Add QSTileLayout Overlays
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config.mk
+++ config.mk
@@ -33,6 +33,59 @@ PRODUCT_COPY_FILES += \
     vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2018-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2018-and-newer.xml \
     vendor/addons/prebuilt/product/etc/sysconfig/preinstalled-packages-product-pixel-2019-and-newer.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-product-pixel-2019-and-newer.xml
 
+# QS layout
+PRODUCT_PACKAGES += \
+	qs_portrait_2x2 \
+	qs_portrait_2x3 \
+	qs_portrait_2x4 \
+	qs_portrait_2x5 \
+	qs_portrait_2x6 \
+	qs_portrait_3x2 \
+	qs_portrait_3x3 \
+	qs_portrait_3x4 \
+	qs_portrait_3x5 \
+	qs_portrait_3x6 \
+	qs_portrait_4x2 \
+	qs_portrait_4x3 \
+	qs_portrait_4x4 \
+	qs_portrait_4x5 \
+	qs_portrait_4x6 \
+	qs_portrait_5x2 \
+	qs_portrait_5x3 \
+	qs_portrait_5x4 \
+	qs_portrait_5x5 \
+	qs_portrait_5x6 \
+	qs_portrait_6x2 \
+	qs_portrait_6x3 \
+	qs_portrait_6x4 \
+	qs_portrait_6x5 \
+	qs_portrait_6x6 \
+	qqs_portrait_1x2 \
+	qqs_portrait_1x3 \
+	qqs_portrait_1x4 \
+	qqs_portrait_1x5 \
+	qqs_portrait_1x6 \
+	qqs_portrait_2x2 \
+	qqs_portrait_2x3 \
+	qqs_portrait_2x4 \
+	qqs_portrait_2x5 \
+	qqs_portrait_2x6 \
+	qqs_portrait_3x2 \
+	qqs_portrait_3x3 \
+	qqs_portrait_3x4 \
+	qqs_portrait_3x5 \
+	qqs_portrait_3x6 \
+	qqs_portrait_4x2 \
+	qqs_portrait_4x3 \
+	qqs_portrait_4x4 \
+	qqs_portrait_4x5 \
+	qqs_portrait_4x6 \
+	qqs_portrait_5x2 \
+	qqs_portrait_5x3 \
+	qqs_portrait_5x4 \
+	qqs_portrait_5x5 \
+	qqs_portrait_5x6
+
 # Fonts
 PRODUCT_PACKAGES += \
     fonts_customization.xml \
