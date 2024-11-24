c1f41a06430d5f8a715920bc0270ffda69d0f300
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-11-24 12:47:22 +0600

    Drop some wifi & signal icon

--- config.mk
+++ config.mk
@@ -288,60 +288,41 @@ PRODUCT_PACKAGES += \
 # Signal Icons
 PRODUCT_PACKAGES += \
     AquariumSignalOverlay \
-    BarsSignalOverlay \
     ButterflySignalOverlay \
     CircleSignalOverlay \
     DaunSignalOverlay \
     DecSignalOverlay \
-    DeepSignalOverlay \
     DoraSignalOverlay \
     EqualSignalOverlay \
     FanSignalOverlay \
     GradiconSignalOverlay \
     HuaweiSignalOverlay \
-    InsideSignalOverlay \
     IosSignalOverlay \
-    MiniSignalOverlay \
     NothingDotSignalOverlay \
-    OdinSignalOverlay \
     PillsSignalOverlay \
-    RelSignalOverlay \
     RomanSignalOverlay \
     RoundSignalOverlay \
     ScrollSignalOverlay \
     SeaSignalOverlay \
-    SneakySignalOverlay \
-    StackSignalOverlay \
-    StrokeSignalOverlay \
     WannuiSignalOverlay \
-    WavySignalOverlay \
-    WindowsSignalOverlay \
     WingSignalOverlay \
     XperiaSignalOverlay \
     ZigZagSignalOverlay \
-	AuroraSignalOverlay \
-	FaintUISignalOverlay \
-	ForlornSignalOverlay \
-	PlumpySignalOverlay
+    AuroraSignalOverlay \
+    PlumpySignalOverlay
 
 # WiFi Icons
 PRODUCT_PACKAGES += \
-    BarsWiFiOverlay \
     DoraWiFiOverlay \
     GradiconWiFiOverlay \
     InsideWiFiOverlay \
     NothingDotWiFiOverlay \
     RoundWiFiOverlay \
-    SneakyWiFiOverlay \
-    StrokeWiFiOverlay \
-    WavyWiFiOverlay \
     WeedWiFiOverlay \
     XperiaWiFiOverlay \
     ZigZagWiFiOverlay \
-	AuroraWiFiOverlay \
-	FaintUIWiFiOverlay \
-	ForlornWiFiOverlay \
-	PlumpyWiFiOverlay
+    AuroraWiFiOverlay \
+    PlumpyWiFiOverlay
 
 # Themes
 PRODUCT_PACKAGES += \
